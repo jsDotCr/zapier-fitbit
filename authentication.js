123e1974fcbd60e0a12a7c9d266fda05318a26fe
jsdotcr <rocco.c@smart-host.co>
2018-02-11 21:04:35 +0100

    feat(authentication): add authentication functions

---
+++ authentication.js
@@ -0,0 +1,116 @@
+const getAccessToken = (z, bundle) => {
+  const clientId = process.env.CLIENT_ID
+  const clientSecret = process.env.CLIENT_SECRET
+  const request = z.request('https://api.fitbit.com/oauth2/token', {
+    method: 'POST',
+    body: {
+      code: bundle.inputData.code,
+      client_id: clientId,
+      client_secret: clientSecret,
+      grant_type: 'authorization_code',
+      state: '{{bundle.inputData.state}}',
+    },
+    headers: {
+      'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+      'content-type': 'application/x-www-form-urlencoded'
+    }
+  })
+
+  // Needs to return at minimum, `access_token`, and if your app also does refresh, then `refresh_token` too
+  return request.then((response) => {
+    if (response.status !== 200) {
+      throw new Error('Unable to fetch access token: ' + response.content);
+    }
+
+    const result = JSON.parse(response.content);
+    return {
+      access_token: result.access_token,
+      refresh_token: result.refresh_token
+    };
+  });
+};
+
+const refreshAccessToken = (z, bundle) => {
+  const clientId = process.env.CLIENT_ID
+  const clientSecret = process.env.CLIENT_SECRET
+  const request = z.request('https://api.fitbit.com/oauth2/token', {
+    method: 'POST',
+    body: {
+      code: bundle.inputData.code,
+      client_id: clientId,
+      client_secret: clientSecret,
+      grant_type: 'refresh_token',
+      state: '{{bundle.inputData.state}}',
+    },
+    headers: {
+      'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+      'content-type': 'application/x-www-form-urlencoded'
+    }
+  })
+
+  // Needs to return `access_token`. If the refresh token stays constant, can skip it. If it changes, can
+  // return it here to update the user's auth on Zapier.
+  return promise.then((response) => {
+    if (response.status !== 200) {
+      throw new Error('Unable to fetch access token: ' + response.content);
+    }
+
+    const result = JSON.parse(response.content);
+    return {
+      access_token: result.access_token
+    };
+  });
+};
+
+const testAuth = (z /*, bundle*/) => {
+  const promise = z.request({
+    method: 'POST',
+    url: 'https://api.fitbit.com/oauth2/introspect',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    }
+  });
+
+  // This method can return any truthy value to indicate the credentials are valid.
+  // Raise an error to show
+  return promise.then((response) => {
+    if (response.status === 401) {
+      throw new Error('The access token you supplied is not valid');
+    }
+    return response;
+  });
+};
+
+module.exports = {
+  type: 'oauth2',
+  oauth2Config: {
+    // Step 1 of the OAuth flow; specify where to send the user to authenticate with your API.
+    // Zapier generates the state and redirect_uri, you are responsible for providing the rest.
+    // Note: can also be a function that returns a string
+    authorizeUrl: {
+      url: 'https://www.fitbit.com/oauth2/authorize',
+      params: {
+        client_id: '{{process.env.CLIENT_ID}}',
+        state: '{{bundle.inputData.state}}',
+        redirect_uri: '{{bundle.inputData.redirect_uri}}',
+        response_type: 'code',
+        scope: '{{process.env.SCOPE}}',
+        expires_in: '2592000'
+      }
+    },
+    // Step 2 of the OAuth flow; Exchange a code for an access token.
+    // This could also use the request shorthand.
+    getAccessToken: getAccessToken,
+    // (Optional) If the access token expires after a pre-defined amount of time, you can implement
+    // this method to tell Zapier how to refresh it.
+    refreshAccessToken: refreshAccessToken,
+    // If you want Zapier to automatically invoke `refreshAccessToken` on a 401 response, set to true
+    autoRefresh: true
+    // If there is a specific scope you want to limit your Zapier app to, you can define it here.
+    // Will get passed along to the authorizeUrl
+    // scope: 'read,write'
+  },
+  // The test method allows Zapier to verify that the access token is valid. We'll execute this
+  // method after the OAuth flow is complete to ensure everything is setup properly.
+  test: testAuth
+};

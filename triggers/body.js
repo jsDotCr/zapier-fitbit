5d444c2c1e52184727d44d3b7d1e03bf313c430e
jsdotcr <rocco.c@smart-host.co>
2018-02-17 20:09:50 +0100

    fix(body): fix base date

--- triggers/body.js
+++ triggers/body.js
@@ -2,7 +2,7 @@ const moment = require('moment')
 
 exports.apiRequest = function apiRequest(what, z, bundle) {
   const today = moment()
-  const oneMonthAgo = today.subtract(1, 'month')
+  const oneMonthAgo = moment().subtract(1, 'month')
   const dataFormat = 'YYYY-MM-DD'
   const responsePromise = z.request({
     url: `https://api.fitbit.com/1/user/-/body/log/${what}/date/${oneMonthAgo.format(dataFormat)}/${today.format(dataFormat)}.json`

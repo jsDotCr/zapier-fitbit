8bc7fc79cece6a187be5a44a93d18d4893293da1
jsDotCr <rocco@js.cr>
2018-02-17 20:09:50 +0100

    fix(body): fix base date

--- test/triggers/bodyFat.js
+++ test/triggers/bodyFat.js
@@ -1,4 +1,5 @@
 /* globals describe it */
+const moment = require('moment')
 const should = require('should')
 const zapier = require('zapier-platform-core')
 const nock = require('nock')
@@ -8,6 +9,34 @@ const appTester = zapier.createAppTester(App)
 zapier.tools.env.inject()
 
 describe('Fitbit App', () => {
+  it('should fetch last month\'s data', done => {
+    const today = moment().format('YYYY-MM-DD')
+    const oneMonthAgo = moment().subtract(1, 'month').format('YYYY-MM-DD')
+    const bundle = { inputData: {} }
+    nock('https://api.fitbit.com/1')
+      .replyContentLength()
+      .get(new RegExp(`/user/-/body/log/fat/date/${oneMonthAgo}/${today}\.json`))
+      .query(bundle.inputData)
+      .reply(200, {
+        "fat": [
+          {
+            "date":"2012-03-05",
+            "fat":14,
+            "logId":1330991999000,
+            "time":"23:59:59",
+            "source": "API"
+          }
+        ]
+      })
+    
+    appTester(App.triggers.bodyFat.operation.perform, bundle)
+      .then(results => {
+        should.exist(results)
+        done()
+      })
+      .catch(done)
+  })
+
   it('should run triggers.bodyFat', done => {
     const bundle = { inputData: {} }
     nock('https://api.fitbit.com/1')
@@ -36,7 +65,6 @@ describe('Fitbit App', () => {
 
     appTester(App.triggers.bodyFat.operation.perform, bundle)
       .then(results => {
-        should.exist(results)
         results.length.should.equal(2)
         done()
       })
